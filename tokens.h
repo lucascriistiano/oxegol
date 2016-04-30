5e7d23eb5d96260ceb266be686b304dddac5cf63
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2016-04-30 11:42:54 -0300

    Insertion of returns

---
+++ tokens.h
@@ -0,0 +1,66 @@
+#define PRINCIPAL       0
+#define PROCEDIMENTO    1
+#define FUNCAO          2
+#define RETORNE         3
+
+#define PARA            4
+#define DE              5
+#define ATE             6
+#define FACA            7
+#define ENQUANTO        8
+#define ESCOLHA         9
+#define CASO            10
+#define CASOCONTRARIO   11
+#define SE              12
+#define SENAO           13
+#define ENTAO           14
+#define PARE            15
+
+#define INTEIRO         16
+#define REAL            17
+#define STRING          18
+#define CARACTERE       19
+#define BOOLEANO        20
+#define BYTE            21
+#define REGISTRO        22
+
+
+#define DOIS_PONTOS     23
+#define PAR_ESQ         24
+#define PAR_DIR         25
+#define CHAVE_ESQ       26
+#define CHAVE_DIR       27
+#define COLCHETE_ESQ    28
+#define COLCHETE_DIR    29
+#define VIRGULA         30
+#define PONTO_E_VIRGULA 31
+#define ATRIBUICAO      32
+
+
+#define RETORNA          33
+#define MAIS             34
+#define MENOS            35
+#define ASTERISCO        36
+#define BARRA            37
+#define MENOR            38
+#define MAIOR            39
+#define MENOR_IGUAL      40
+#define MAIOR_IGUAL      41
+#define IGUAL            42
+#define MOD              43
+#define E_BITS           44
+#define OU_BITS          45
+#define DESLOCAMENTO_DIR 46
+#define DESLOCAMENTO_ESQ 47
+
+#define E_LOGICO         48
+#define OU_LOGICO        49
+#define NAO_LOGICO       50
+
+#define LITERAL_INTEIRO   51
+#define LITERAL_REAL      52
+#define LITERAL_BOOLEANO  53
+#define LITERAL_STRING    54
+#define LITERAL_CARACTERE 55
+
+#define ID                56
