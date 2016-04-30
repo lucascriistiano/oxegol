c8657fdbda657a3f1fb3360032459a6d8fc62f08
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2016-04-30 11:58:30 -0300

    Correction of return values

--- tokens.h
+++ tokens.h
@@ -1,66 +1,66 @@
-#define PRINCIPAL       0
-#define PROCEDIMENTO    1
-#define FUNCAO          2
-#define RETORNE         3
+#define PRINCIPAL       10
+#define PROCEDIMENTO    11
+#define FUNCAO          12
+#define RETORNE         13
 
-#define PARA            4
-#define DE              5
-#define ATE             6
-#define FACA            7
-#define ENQUANTO        8
-#define ESCOLHA         9
-#define CASO            10
-#define CASOCONTRARIO   11
-#define SE              12
-#define SENAO           13
-#define ENTAO           14
-#define PARE            15
+#define PARA            14
+#define DE              15
+#define ATE             16
+#define FACA            17
+#define ENQUANTO        18
+#define ESCOLHA         19
+#define CASO            20
+#define CASOCONTRARIO   21
+#define SE              22
+#define SENAO           23
+#define ENTAO           24
+#define PARE            25
 
-#define INTEIRO         16
-#define REAL            17
-#define STRING          18
-#define CARACTERE       19
-#define BOOLEANO        20
-#define BYTE            21
-#define REGISTRO        22
-
-
-#define DOIS_PONTOS     23
-#define PAR_ESQ         24
-#define PAR_DIR         25
-#define CHAVE_ESQ       26
-#define CHAVE_DIR       27
-#define COLCHETE_ESQ    28
-#define COLCHETE_DIR    29
-#define VIRGULA         30
-#define PONTO_E_VIRGULA 31
-#define ATRIBUICAO      32
+#define INTEIRO         26
+#define REAL            27
+#define STRING          28
+#define CARACTERE       29
+#define BOOLEANO        30
+#define BYTE            31
+#define REGISTRO        32
 
 
-#define RETORNA          33
-#define MAIS             34
-#define MENOS            35
-#define ASTERISCO        36
-#define BARRA            37
-#define MENOR            38
-#define MAIOR            39
-#define MENOR_IGUAL      40
-#define MAIOR_IGUAL      41
-#define IGUAL            42
-#define MOD              43
-#define E_BITS           44
-#define OU_BITS          45
-#define DESLOCAMENTO_DIR 46
-#define DESLOCAMENTO_ESQ 47
+#define DOIS_PONTOS     33
+#define PAR_ESQ         34
+#define PAR_DIR         35
+#define CHAVE_ESQ       36
+#define CHAVE_DIR       37
+#define COLCHETE_ESQ    38
+#define COLCHETE_DIR    39
+#define VIRGULA         40
+#define PONTO_E_VIRGULA 41
+#define ATRIBUICAO      42
 
-#define E_LOGICO         48
-#define OU_LOGICO        49
-#define NAO_LOGICO       50
 
-#define LITERAL_INTEIRO   51
-#define LITERAL_REAL      52
-#define LITERAL_BOOLEANO  53
-#define LITERAL_STRING    54
-#define LITERAL_CARACTERE 55
+#define RETORNA          43
+#define MAIS             44
+#define MENOS            45
+#define ASTERISCO        46
+#define BARRA            47
+#define MENOR            48
+#define MAIOR            49
+#define MENOR_IGUAL      50
+#define MAIOR_IGUAL      51
+#define IGUAL            52
+#define MOD              53
+#define E_BITS           54
+#define OU_BITS          55
+#define DESLOCAMENTO_DIR 56
+#define DESLOCAMENTO_ESQ 57
 
-#define ID                56
+#define E_LOGICO         58
+#define OU_LOGICO        59
+#define NAO_LOGICO       60
+
+#define LITERAL_INTEIRO   61
+#define LITERAL_REAL      62
+#define LITERAL_BOOLEANO  63
+#define LITERAL_STRING    64
+#define LITERAL_CARACTERE 65
+
+#define ID                66
