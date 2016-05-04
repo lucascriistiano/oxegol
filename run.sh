fad0ec07869f4c376ef7aa50230d8a8aa44f198f
Stefano Momo Loss <momoloss10@gmail.com>
2016-05-04 12:25:45 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Compilacao e execucao
+# Compilacao e execucao lex (flex)
 flex oxegol.l
 gcc lex.yy.c -ll
 ./a.out < quicksort.oxe
+
+# Compilação yahcc (bison)
+bison -v -d oxegol.y
 
 # Limpeza de arquivos criados
 rm lex.yy.c
