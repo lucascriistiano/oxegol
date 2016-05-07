50d4c75f67e60bc5cef10a734a53dd629a4e236a
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2016-05-06 22:15:20 -0300

    Atualização do arquivo de execução

--- run.sh
+++ run.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-# V1
-# # Compilacao e execucao
-# flex oxegol.l
-# gcc lex.yy.c -ll
-# ./a.out < quicksort.oxe
-
-# # Limpeza de arquivos criados
-# rm lex.yy.c
-# rm a.out
-
-# V2
 # Compilacao e execucao
 flex oxegol.l
 yacc -d -v oxegol.y
 cc lex.yy.c y.tab.c -o programa.oxe
-./programa.oxe < teste.oxe
+./programa.oxe < quicksort.oxe
 
 # Limpeza de arquivos criados
 rm lex.yy.c
-rm y.output
+# rm y.output
 rm y.tab.c
 rm y.tab.h
