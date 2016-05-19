e33ca1cc99aaa58b35f3e175c798c465b8740f85
lucascristiano27 <lucascristiano27@gmail.com>
2016-05-19 17:56:16 +0000

    Primeira versão da Hash table

---
+++ testeHash.c
@@ -0,0 +1,22 @@
+#include "hash.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+
+int main( int argc, char **argv ) {
+
+	hashtable_t *hashtable = ht_create( 65536 );
+
+	ht_set( hashtable, "key1", "inky" );
+	ht_set( hashtable, "key2", "pinky" );
+	ht_set( hashtable, "key3", "blinky" );
+	ht_set( hashtable, "key4", "floyd" );
+
+	printf( "%s\n", ht_get( hashtable, "key1" ) );
+	printf( "%s\n", ht_get( hashtable, "key2" ) );
+	printf( "%s\n", ht_get( hashtable, "key3" ) );
+	printf( "%s\n", ht_get( hashtable, "key4" ) );
+
+	return 0;
+}
